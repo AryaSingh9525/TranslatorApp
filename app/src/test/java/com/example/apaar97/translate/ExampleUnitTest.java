ba5aa059974a14f0f8a3e9e2d9a9faadc5e3148e
AryaSingh9525 <AryaSingh9525@users.noreply.github.com>
2020-07-13 15:35:00 +0530

    Online Offline Translator App

---
+++ app/src/test/java/com/example/apaar97/translate/ExampleUnitTest.java
@@ -0,0 +1,13 @@
+package com.example.apaar97.translate;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
